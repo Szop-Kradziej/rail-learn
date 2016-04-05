3bebdf68b5020647e479003295d9893d6e3edb42
Szop-kradziej <karolina.drabarz@gmail.com>
2016-04-06 00:48:48 +0200

    Create tripsGroupView and add adapter

--- app/src/main/java/com/drabarz/karola/raillearn/TripsActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/TripsActivity.java
@@ -2,12 +2,24 @@ package com.drabarz.karola.raillearn;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.drabarz.karola.raillearn.adapter.TripsGroupAdapter;
 
 public class TripsActivity extends AppCompatActivity{
+
+    private TripsGroupAdapter tripsGroupAdapter = new TripsGroupAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trips_activity);
+
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        ListView tripsGroupView = (ListView) findViewById(R.id.tripsGroupView);
+        tripsGroupView.setAdapter(tripsGroupAdapter);
     }
 }
