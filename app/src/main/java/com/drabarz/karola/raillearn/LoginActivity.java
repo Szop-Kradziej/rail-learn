896d7a8946fd93c94a95b74381966eaa40d7e6a2
Szop-kradziej <karolina.drabarz@gmail.com>
2016-04-06 00:14:29 +0200

    Open new TripsActivity after button clicked.

--- app/src/main/java/com/drabarz/karola/raillearn/LoginActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.drabarz.karola.raillearn;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -12,5 +13,22 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        bindTripsButtonAction();
+    }
+
+    private void bindTripsButtonAction() {
+        Button tripsButton = (Button) findViewById(R.id.tripsButton);
+        tripsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTripsButtonClick();
+            }
+        });
+    }
+
+    private void onTripsButtonClick() {
+        Intent intent = new Intent(this, TripsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/drabarz/karola/raillearn/TripsActivity.java
@@ -0,0 +1,13 @@
+package com.drabarz.karola.raillearn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TripsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trips_activity);
+    }
+}
