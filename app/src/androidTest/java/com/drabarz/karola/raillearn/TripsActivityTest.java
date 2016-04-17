a0407cb1fdb9fc8b7ca3b3c0168e9e9b83f8749a
Szop-kradziej <karolina.drabarz@gmail.com>
2016-04-17 23:07:40 +0200

    Add test shouldStartFullTripsActivity.

---
+++ app/src/androidTest/java/com/drabarz/karola/raillearn/TripsActivityTest.java
@@ -0,0 +1,26 @@
+package com.drabarz.karola.raillearn;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import com.drabarz.karola.raillearn.trip.list.TripsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TripsActivityTest {
+
+    @Rule
+    public ActivityTestRule activityTestRule = new ActivityTestRule<>(TripsActivity.class);
+
+    @Test
+    public void shouldStartFullTripsActivity() throws InterruptedException {
+        Thread.sleep(4000);
+        Espresso.onView(ViewMatchers.withId(R.id.tripFieldLayout)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.tripDescriptionTextView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+}
