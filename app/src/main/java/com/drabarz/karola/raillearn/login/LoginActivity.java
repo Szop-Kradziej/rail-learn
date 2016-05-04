9fe83f9d74065a92234b1d8ff1e4b99dfdc6550d
Szop-kradziej <karolina.drabarz@gmail.com>
2016-05-04 17:40:42 +0200

    Add log in with facebook.

--- app/src/main/java/com/drabarz/karola/raillearn/login/LoginActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/login/LoginActivity.java
@@ -1,35 +1,55 @@
 package com.drabarz.karola.raillearn.login;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 
 import com.drabarz.karola.raillearn.R;
 import com.drabarz.karola.raillearn.trip.list.TripsActivity;
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends ActionBarActivity {
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.login_activity);
 
-        bindTripsButtonAction();
-    }
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
 
-    private void bindTripsButtonAction() {
-        Button tripsButton = (Button) findViewById(R.id.tripsButton);
-        tripsButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-                onTripsButtonClick();
+            public void onSuccess(LoginResult loginResult) {
+                TripsActivity.start(LoginActivity.this);
+            }
+
+            @Override
+            public void onCancel() {
+                // Do nothing
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.e("LoginActivity", "Facebook login error: " + e, e);
             }
         });
     }
 
-    private void onTripsButtonClick() {
-        TripsActivity.start(this);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
