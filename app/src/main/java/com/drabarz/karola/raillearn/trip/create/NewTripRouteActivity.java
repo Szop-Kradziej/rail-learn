b0e3e8c7fddc24c7b43e70956d447f38f5d592a3
Szop-kradziej <karolina.drabarz@gmail.com>
2016-05-17 00:51:02 +0200

    Refactor - add abstract TripRouteActivity.

--- app/src/main/java/com/drabarz/karola/raillearn/trip/create/NewTripRouteActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/trip/create/NewTripRouteActivity.java
@@ -2,91 +2,14 @@ package com.drabarz.karola.raillearn.trip.create;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.drabarz.karola.raillearn.R;
-import com.drabarz.karola.raillearn.model.Departure;
 import com.drabarz.karola.raillearn.model.Route;
 
-public class NewTripRouteActivity extends AppCompatActivity{
+public class NewTripRouteActivity extends TripRouteActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_trip_route_activity);
-
-        addDateTextListener();
-        addTimeTextListener();
-        addConfirmRouteButtonListener();
-    }
-
-    private void addDateTextListener() {
-        EditText inputStationDateEditText = (EditText) findViewById(R.id.inputDepartureStationDateEditText);
-        inputStationDateEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDatePickerDialog();
-            }
-        });
-    }
-
-    public void showDatePickerDialog() {
-        DatePicker datePicker = new DatePicker();
-        datePicker.show(getSupportFragmentManager(), "datePicker");
-    }
-
-    private void addTimeTextListener() {
-        EditText inputStationTimeEditText = (EditText) findViewById(R.id.inputDepartureStationTimeEditText);
-        inputStationTimeEditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showTimePickerDialog();
-            }
-        });
-    }
-
-    private void showTimePickerDialog() {
-        TimePicker timePicker = new TimePicker();
-        timePicker.show(getSupportFragmentManager(), "timePicker");
-    }
-
-    private void addConfirmRouteButtonListener() {
-        Button confirmRouteButton = (Button) findViewById(R.id.confirmRouteButton);
-        confirmRouteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onConfirmRouteButtonClicked();
-            }
-        });
-    }
-
-    private void onConfirmRouteButtonClicked() {
+    protected void onConfirmRouteButtonClicked() {
         Route route = getRouteInputData();
         startNewTripOfferActivity(route);
-    }
-
-    private Route getRouteInputData() {
-        Departure departure = getDepartureInputData();
-        String arrival = getArrivalInputData();
-        return new Route(departure, arrival);
-    }
-
-    public Departure getDepartureInputData() {
-        String name = ((EditText) findViewById(R.id.inputDepartureStationNameEditText)).getText().toString();
-        String date = ((EditText) findViewById(R.id.inputDepartureStationDateEditText)).getText().toString();
-        String time = ((EditText) findViewById(R.id.inputDepartureStationTimeEditText)).getText().toString();
-
-        return new Departure(name, date, time);
-    }
-
-    private String getArrivalInputData() {
-        String arrival = ((EditText) findViewById(R.id.inputArrivalStationNameEditText)).getText().toString();
-
-        return arrival;
     }
 
     private void startNewTripOfferActivity(Route route) {
