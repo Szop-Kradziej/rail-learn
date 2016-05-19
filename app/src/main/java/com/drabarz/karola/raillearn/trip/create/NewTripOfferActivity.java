8e7797945eb02134f66df4e0b2fe389680518afb
Szop-kradziej <karolina.drabarz@gmail.com>
2016-05-19 17:48:22 +0200

    Refactor - make Trip Offer Activity abstract.

--- app/src/main/java/com/drabarz/karola/raillearn/trip/create/NewTripOfferActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/trip/create/NewTripOfferActivity.java
@@ -4,37 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.drabarz.karola.raillearn.R;
-import com.drabarz.karola.raillearn.model.Offer;
 import com.drabarz.karola.raillearn.model.Route;
 import com.drabarz.karola.raillearn.model.Trip;
-import com.drabarz.karola.raillearn.model.User;
 import com.drabarz.karola.raillearn.trip.details.NewFullTripActivity;
 
-public class NewTripOfferActivity extends AppCompatActivity {
-
-    private static final String ROUTE = "route";
-    private static final String DESCRIPTION = "description";
-    private Route route;
+public class NewTripOfferActivity extends TripOfferActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_trip_offer_activity);
-
-        route = (Route) getIntent().getSerializableExtra(ROUTE);
 
         if(isDescriptionSaved()) {
             setSavedDescription();
         }
-
-        addConfirmOfferButtonListener();
     }
 
     private boolean isDescriptionSaved() {
@@ -47,44 +32,10 @@ public class NewTripOfferActivity extends AppCompatActivity {
         inputTripDescriptionEditText.setText(description);
     }
 
-    private void addConfirmOfferButtonListener() {
-        Button confirmOfferButton = (Button) findViewById(R.id.confirmOfferButton);
-        confirmOfferButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onConfirmOfferButtonClicked();
-            }
-        });
-    }
-
-    private void onConfirmOfferButtonClicked() {
+    protected void onConfirmOfferButtonClicked() {
         Trip trip = createTripFromInputData();
         saveDescription(trip.getOffer().getDescription());
         startNewFullTripActivity(trip);
-    }
-
-    private Trip createTripFromInputData() {
-        Offer offer = getOfferInputData();
-        String userId = getUserId();
-        Log.i("NewTripOfferAct", "User id: " + userId);
-        User user = new User("default_name", userId);
-
-        return new Trip(user, offer, route);
-    }
-
-    private Offer getOfferInputData() {
-        String title = ((EditText) findViewById(R.id.inputTripTitleEditText)).getText().toString();
-        String description = ((EditText) findViewById(R.id.inputTripDescriptionEditText)).getText().toString();
-
-        return new Offer(title, description);
-    }
-
-    private String getUserId() {
-        return PreferenceManager.getDefaultSharedPreferences(this).getString("user_id", null);
-    }
-
-    private void saveDescription(String description) {
-        PreferenceManager.getDefaultSharedPreferences(this).edit().putString(DESCRIPTION,description).commit();
     }
 
     private void startNewFullTripActivity(Trip trip) {
---
+++ app/src/main/java/com/drabarz/karola/raillearn/trip/create/TripOfferActivity.java
@@ -0,0 +1,67 @@
+package com.drabarz.karola.raillearn.trip.create;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.drabarz.karola.raillearn.R;
+import com.drabarz.karola.raillearn.model.Offer;
+import com.drabarz.karola.raillearn.model.Route;
+import com.drabarz.karola.raillearn.model.Trip;
+import com.drabarz.karola.raillearn.model.User;
+
+public abstract class TripOfferActivity extends AppCompatActivity {
+    protected static final String ROUTE = "route";
+    protected static final String DESCRIPTION = "description";
+    protected Route route;
+
+    protected abstract void onConfirmOfferButtonClicked();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_trip_offer_activity);
+
+        route = (Route) getIntent().getSerializableExtra(ROUTE);
+
+        addConfirmOfferButtonListener();
+    }
+
+    protected void addConfirmOfferButtonListener() {
+        Button confirmOfferButton = (Button) findViewById(R.id.confirmOfferButton);
+        confirmOfferButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onConfirmOfferButtonClicked();
+            }
+        });
+    }
+
+    protected Trip createTripFromInputData() {
+        Offer offer = getOfferInputData();
+        String userId = getUserId();
+        Log.i("NewTripOfferAct", "User id: " + userId);
+        User user = new User("default_name", userId);
+
+        return new Trip(user, offer, route);
+    }
+
+    private Offer getOfferInputData() {
+        String title = ((EditText) findViewById(R.id.inputTripTitleEditText)).getText().toString();
+        String description = ((EditText) findViewById(R.id.inputTripDescriptionEditText)).getText().toString();
+
+        return new Offer(title, description);
+    }
+
+    private String getUserId() {
+        return PreferenceManager.getDefaultSharedPreferences(this).getString("user_id", null);
+    }
+
+    protected void saveDescription(String description) {
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putString(DESCRIPTION,description).commit();
+    }
+}
