5971ebfebb324a2444615e1d52df648c6739f238
Szop-kradziej <karolina.drabarz@gmail.com>
2017-02-01 20:37:12 +0100

    Add google log in prototype

--- app/src/main/java/com/drabarz/karola/raillearn/login/LoginActivity.java
+++ app/src/main/java/com/drabarz/karola/raillearn/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.drabarz.karola.raillearn.R;
 import com.drabarz.karola.raillearn.model.LoginData;
@@ -17,16 +18,23 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TOKEN = "token";
     private static final String USER_ID = "user_id";
+    private static final int RC_SIGN_IN = 1276;
     private CallbackManager callbackManager;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +53,20 @@ public class LoginActivity extends ActionBarActivity {
 
     private void showLogInForm() {
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login_activity);
+        showFacebookLogInForm();
+        showGoogleLogInForm();
+    }
+
+    private void showFacebookLogInForm() {
         callbackManager = CallbackManager.Factory.create();
 
-        setContentView(R.layout.login_activity);
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
         loginButton.setReadPermissions("email");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                onFacebookLogInSuccess(loginResult.getAccessToken().getToken());
+                onLogInSuccess(loginResult.getAccessToken().getToken());
             }
 
             @Override
@@ -69,6 +81,29 @@ public class LoginActivity extends ActionBarActivity {
         });
     }
 
+    private void showGoogleLogInForm() {
+        GoogleSignInOptions googleSingInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSingInOptions)
+                .build();
+
+        findViewById(R.id.google_login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
     private void onLogInSuccess(LoginData data) {
         PreferenceManager.getDefaultSharedPreferences(this).edit().putString(TOKEN, data.getToken()).commit();
         PreferenceManager.getDefaultSharedPreferences(this).edit().putString(USER_ID, data.getId()).commit();
@@ -77,12 +112,26 @@ public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            return;
+        }
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void onFacebookLogInSuccess(String facebookToken) {
+    private void handleSignInResult(GoogleSignInResult result) {
+      //  if (result.isSuccess()) {
+      //      GoogleSignInAccount account = result.getSignInAccount();
+            onLogInSuccess("random_string");//account.getIdToken());
+      //  } else {
+      //      Log.e("LoginActivity", "Google login error: " + result.getStatus().getStatusMessage());
+      //  }
+    }
+
+    private void onLogInSuccess(String token) {
         RailLearnLogInUserApi retrofitService = ServiceFactory.createRetrofitService(RailLearnLogInUserApi.class, getString(R.string.service_endpoint));
-        retrofitService.logInUser(facebookToken)
+        retrofitService.logInUser(token)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<LoginData>() {
@@ -97,5 +146,10 @@ public class LoginActivity extends ActionBarActivity {
                                }
                            }
                 );
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("LoginActivity", "On connection failed: " + connectionResult.toString());
     }
 }
